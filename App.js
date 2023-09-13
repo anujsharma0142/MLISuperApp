a07a8ec8a581de32ef65b9c8002c8e2ed2d9d95d
guptatejas65@gmail.com <guptatejas65@gmail.com>
2023-09-13 21:42:47 +0530

    FIXED DEPENDENCIES

--- App.js
+++ App.js
@@ -1,12 +1,9 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import AppNavigation from './src/components/AppNavigation';
 
 const App = () => {
-  return (
-    <View>
-      <Text>App</Text>
-    </View>
-  );
+  return <AppNavigation />;
 };
 
 export default App;
