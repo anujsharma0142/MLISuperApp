ba443dc615249cf0821b4380531becd74c9f78ff
guptatejas65@gmail.com <guptatejas65@gmail.com>
2023-09-13 21:39:46 +0530

    i18n dependencies fixed.

---
+++ locals/english.js
@@ -0,0 +1,7 @@
+export default {
+  welcomeMessage: 'Welcome To The SuperMAX',
+  mPro: 'VYMO',
+  mPower: 'mPower',
+  mQuote: 'mQuote',
+  maxlifeLite: 'MLO',
+};
---
+++ locals/hindi.js
@@ -0,0 +1,7 @@
+export default {
+  welcomeMessage: 'सुपरमैक्स में आपका स्वागत है',
+  mPro: 'वायमो',
+  mPower: 'एम पॉवर',
+  mQuote: 'एम कोट',
+  maxlifeLite: 'एमएलओ',
+};
