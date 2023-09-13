ffb726f6dbbbe6d24ee3b794ac033aa9ef2d4756
imanujsharma99 <imanujsharma999@gmail.com>
2023-09-13 16:22:57 +0530

    push notification

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import PushNotification from './src/components/PushNotification/PushNotification'
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+      <PushNotification/>
   )
 }
 
