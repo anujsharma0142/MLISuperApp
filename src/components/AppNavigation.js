2a278f35c38a03d17c28a315605cc2a61532e440
Vandna Patidar <vandna@monocept.com>
2023-09-13 16:27:02 +0530

    changes calender | imageUploading |infiniteScrolling

--- src/components/AppNavigation.js
+++ src/components/AppNavigation.js
@@ -1,8 +1,12 @@
-import { View, Text } from 'react-native'
+import { View, Text,ScrollView } from 'react-native'
 import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { NavigationContainer } from '@react-navigation/native';
 import Calendar from './screens/Calendar';
+import Index from './screens/Index';
+import Card from './screens/Card';
+import ImageUpload from './ImageUpload';
+import InfiniteScrolling from './screens/InfinityScrolling';
 
 const stack = createNativeStackNavigator();
 
@@ -10,7 +14,11 @@ const AppNavigation = () => {
   return (
    <NavigationContainer>
     <stack.Navigator>
+     
+      <stack.Screen name='Main' component={Index} />
         <stack.Screen name='Calendar' component={Calendar}/>
+        <stack.Screen name='OfflineImageUpload' component={ImageUpload} />
+        <stack.Screen name='InfinityScrolling' component={InfiniteScrolling} />
     </stack.Navigator>
    </NavigationContainer>
   )
--- src/components/screens/Index.js
+++ src/components/screens/Index.js
@@ -1,10 +1,44 @@
-import { View, Text, Button } from 'react-native'
+import { View, Text, Button ,ScrollView} from 'react-native'
 import React from 'react'
+import Card from './Card';
+import Calendar from './Calendar';
 
-const Index = ([props]) => {
+const Index = ({navigation}) => {
+  // const cardData = [
+  //   { title: 'Card 1' },
+  //   { title: 'Card 2' },
+  //   { title: 'Card 3' },
+  //   { title: 'Card 4' },
+  //   { title: 'Card 5' },
+  //   { title: 'Card 6' },
+  // ];
+
+  // return (
+  //   <ScrollView >
+  //     <View >
+  //       {cardData.slice(0, 3).map((card, index) => (
+  //         <Card key={index} title={card.title} />
+  //       ))}
+  //     </View>
+  //     <View >
+  //       {cardData.slice(3, 6).map((card, index) => (
+  //         <Card key={index} title={card.title} />
+  //       ))}
+  //     </View>
+  //   </ScrollView>
+  // );
   return (
     <View>
-     <Button title='Calendar' ></Button>
+     <Button title='Calendar' onPress={() =>
+        navigation.navigate('Calendar')} ></Button>
+     <Button title='PushNotification' ></Button>
+      <Button title='RealTimeDataBase' ></Button>
+     <Button title='OfflineLocalStorage' onPress={() =>
+        navigation.navigate('OfflineImageUpload')}></Button>
+     <Button title='Super' ></Button>
+     <Button title='InfiniteScrolling' ></Button>
+     <Button title='SuperMaxWithDeeplinking' ></Button>
+    
     </View>
   )
 }
